f38f6f9712bb947531a48e09461ce2dea57757c1
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-10 17:10:48 +0200

    Update MainActivity.java

--- app/src/main/java/com/terminal/app/MainActivity.java
+++ app/src/main/java/com/terminal/app/MainActivity.java
@@ -3,7 +3,7 @@ package com.terminal.app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.view.SoundEffectConstants; // Для звука
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,40 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 1. Пытаемся найти макет
         int layoutId = getResources().getIdentifier("activity_main", "layout", getPackageName());
-        
-        if (layoutId == 0) {
-            Toast.makeText(this, "КРИТИЧЕСКАЯ ОШИБКА: activity_main.xml не найден!", Toast.LENGTH_LONG).show();
-            return;
-        }
-        
-        setContentView(layoutId);
+        if (layoutId != 0) setContentView(layoutId);
 
-        // 2. Инициализируем мозги
         interpreter = new Interpreter(this);
 
-        // 3. Ищем кнопки по именам
-        int edId = getResources().getIdentifier("editor", "id", getPackageName());
-        int bStartId = getResources().getIdentifier("btnStart", "id", getPackageName());
-        int bStopId = getResources().getIdentifier("btnStop", "id", getPackageName());
+        EditText editor = findViewById(getResources().getIdentifier("editor", "id", getPackageName()));
+        View btnStart = findViewById(getResources().getIdentifier("btnStart", "id", getPackageName()));
+        View btnStop = findViewById(getResources().getIdentifier("btnStop", "id", getPackageName()));
 
-        EditText editor = findViewById(edId);
-        View btnStart = findViewById(bStartId);
-        View btnStop = findViewById(bStopId);
-
-        // 4. Проверяем, нашлись ли они
-        if (btnStart == null || editor == null) {
-            Toast.makeText(this, "ОШИБКА: Кнопки не найдены в XML!", Toast.LENGTH_LONG).show();
-        } else {
+        if (btnStart != null && editor != null) {
             btnStart.setOnClickListener(v -> {
-                String code = editor.getText().toString();
-                interpreter.run(code);
+                v.playSoundEffect(SoundEffectConstants.CLICK); // ЗВУК
+                interpreter.run(editor.getText().toString(), editor);
             });
         }
 
         if (btnStop != null) {
-            btnStop.setOnClickListener(v -> interpreter.stop());
+            btnStop.setOnClickListener(v -> {
+                v.playSoundEffect(SoundEffectConstants.CLICK); // ЗВУК
+                interpreter.stop();
+            });
         }
     }
 }
